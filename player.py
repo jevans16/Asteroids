6f66ce3253dce3cae358dd76b592ed516337b008
jevans16 <jade-evans@hotmail.co.uk>
2026-09-10 22:21:51 +0100

    added destruction on shot

--- player.py
+++ player.py
@@ -10,6 +10,7 @@ class Player(CircleShape):
     def __init__(self, x: float, y: float) -> None:
         super().__init__(x, y, constants.PLAYER_RADIUS)
         self.rotation: float = 0
+        self.shot_cooldown: float = 0
 
     def triangle(self) -> list[pygame.Vector2]:
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
@@ -45,6 +46,8 @@ class Player(CircleShape):
         if keys[pygame.K_SPACE]:
             self.shoot()
 
+        self.shot_cooldown -= dt
+
     def move(self, dt) -> None:
         unit_vector = pygame.Vector2(0, 1)
         rotated_vector = unit_vector.rotate(self.rotation)
@@ -52,5 +55,8 @@ class Player(CircleShape):
         self.position += rotated_with_speed_vector
 
     def shoot(self) -> None:
+        if self.shot_cooldown > 0:
+            return
+        self.shot_cooldown = constants.PLAYER_SHOOT_COOLDOWN_SECONDS
         shot = Shot(self.position.x, self.position.y)
         shot.velocity = pygame.Vector2(0, 1).rotate(self.rotation)*constants.PLAYER_SHOOT_SPEED
