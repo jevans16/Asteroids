4db1f3cd6e76f39e21e45d3228e7bc7c2af5018a
jevans16 <jade-evans@hotmail.co.uk>
2026-09-10 22:44:00 +0100

    added split functionality upon hit

--- asteroid.py
+++ asteroid.py
@@ -1,8 +1,9 @@
 from typing import override
-
+import random
 from circleshape import CircleShape
+from logger import log_event
 import pygame
-from constants import LINE_WIDTH
+from constants import LINE_WIDTH, ASTEROID_MIN_RADIUS
 
 class Asteroid(CircleShape):
 
@@ -20,3 +21,18 @@ class Asteroid(CircleShape):
         self.position += self.velocity * dt
 
         return super().update(dt)
+
+    def split(self) -> None:
+        self.kill()
+        if self.radius <= ASTEROID_MIN_RADIUS:
+            return
+        else:
+            log_event("asteroid_split")
+            new_angle =random.uniform(20, 50)
+            first_velocity = self.velocity.rotate(new_angle)
+            second_velocity = self.velocity.rotate(-new_angle)
+            new_radius = self.radius - ASTEROID_MIN_RADIUS
+            asteroid_1 = Asteroid(self.position.x, self.position.y, new_radius)
+            asteroid_2 = Asteroid(self.position.x, self.position.y, new_radius)
+            asteroid_1.velocity = first_velocity * 1.2
+            asteroid_2.velocity = second_velocity * 1.2
--- main.py
+++ main.py
@@ -46,7 +46,7 @@ def main():
                 if asteroid.collides_with(shot):
                     log_event("asteroid_shot")
                     shot.kill()
-                    asteroid.kill()
+                    asteroid.split()
         screen.fill("black")
         for item in drawable:
             item.draw(screen)
