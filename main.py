cc4af244511697aa35dc6b7a80fa809269c1f0c1
jevans16 <jade-evans@hotmail.co.uk>
2026-09-10 18:46:19 +0100

    added player triangle

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import player
 import pygame
 import constants
 from logger import log_state
@@ -12,6 +13,7 @@ def main():
     screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0.0
+    player_1 = player.Player(constants.SCREEN_WIDTH/2, constants.SCREEN_HEIGHT/2)
 
     while True:
         log_state()
@@ -19,6 +21,7 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        player_1.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
 
---
+++ player.py
@@ -0,0 +1,24 @@
+from typing import override
+
+import pygame
+import constants
+from circleshape import CircleShape
+
+class Player(CircleShape):
+
+    def __init__(self, x: float, y: float) -> None:
+        super().__init__(x, y, constants.PLAYER_RADIUS)
+        self.rotation: float = 0
+
+    def triangle(self) -> list[pygame.Vector2]:
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        right = pygame.Vector2(0, 1).rotate(self.rotation + 90) * self.radius / 1.5
+        a = self.position + forward * self.radius
+        b = self.position - forward * self.radius - right
+        c = self.position - forward * self.radius + right
+        return [a, b, c]
+
+    @override
+    def draw(self, screen: pygame.Surface) -> None:
+        pygame.draw.polygon(screen, "white", self.triangle(), constants.LINE_WIDTH)
+        return super().draw(screen)
